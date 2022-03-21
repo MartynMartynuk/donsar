45ba257de8cd7726bc305c7b8aedc74f0c93da8e
Martyn <ma.ahmatov@tensor.ru>
2022-03-21 12:38:53 +0400

    Initial

---
+++ donsar_system/boric_acid_concentration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BoricAcidConcentrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'boric_acid_concentration'
