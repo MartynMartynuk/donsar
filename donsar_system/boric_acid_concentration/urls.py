2c8d82bad8a0da98bea3d2a10c38b9e41fc2e829
Martyn <ma.ahmatov@tensor.ru>
2022-03-21 20:04:00 +0400

    Create template

--- donsar_system/boric_acid_concentration/urls.py
+++ donsar_system/boric_acid_concentration/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from . import views
 from django.urls import path
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.bor_calc_page),
+    # path('add/', name='bor_add_album')
+]
--- donsar_system/boric_acid_concentration/views.py
+++ donsar_system/boric_acid_concentration/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def bor_calc_page(request):
+    return render(request, template_name='bor_calc_page.html')
