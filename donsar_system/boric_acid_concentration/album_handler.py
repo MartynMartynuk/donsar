0dfe1debe4cc386d3ef182016340317b9d35de94
Martyn <ma.ahmatov@tensor.ru>
2022-03-23 23:13:34 +0400

    Tuned calculation 2

--- donsar_system/boric_acid_concentration/album_handler.py
+++
@@ -1,4 +0,0 @@
-def album_handle(album):
-    with open('file.txt', 'wb+') as destination:
-        for chunk in album.chunks():
-            destination.write(chunk)
--- donsar_system/boric_acid_concentration/views.py
+++ donsar_system/boric_acid_concentration/views.py
@@ -20,9 +20,10 @@ def bor_calc_page(request):
     if request.method == 'POST':
         form = BorCalcForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
             result = BorCalculator.returner(form.cleaned_data['param_1'],
                                             form.cleaned_data['param_2'])
+            return render(request, 'bor_calculator/bor_calc_page.html',
+                          {'form': form, 'result': result})
     else:
         form = BorCalcForm()
-    return render(request, 'bor_calculator/bor_calc_page.html', {'form': form, 'result': result})
+    return render(request, 'bor_calculator/bor_calc_page.html', {'form': form})
