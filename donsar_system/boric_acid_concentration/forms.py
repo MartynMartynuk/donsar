0d2192754da27e22427617eb9fc10be93ca8aef4
Martyn <asd@mail.ru>
2022-03-30 12:22:20 +0400

    Win PC

--- donsar_system/boric_acid_concentration/forms.py
+++ donsar_system/boric_acid_concentration/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import BorCalculator, Album, Block
+from .models import *
 
 
 class AddAlbumForm(forms.ModelForm):
