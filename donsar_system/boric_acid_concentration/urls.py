72f873ebf22d8426ac4c4d8f1f87752658c91262
Martyn <ma.ahmatov@tensor.ru>
2022-06-12 12:16:05 +0400

    Убрал login_required

--- donsar_system/boric_acid_concentration/urls.py
+++ donsar_system/boric_acid_concentration/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.bor_calc_resume_page, name='bor_calc'),
     path('start_calc/', views.bor_calc_start_page, name='bor_calc_start'),
-    path('add/', views.add_album_page, name='add_album'),
+    path('add_album/', views.add_album_page, name='add_album'),
     path('graph/', views.graph_page, name='graph'),
     path('add_points/', views.add_points, name='add_points'),
     path('login/', views.login_page, name='login'),
--- donsar_system/boric_acid_concentration/views.py
+++ donsar_system/boric_acid_concentration/views.py
@@ -13,40 +13,42 @@ from .water_exchange_function import *
 from donsar_system.settings import DATE_INPUT_FORMATS
 
 
-@login_required(login_url='/login/')
 def add_album_page(request):
     """
     Страница добавления альбома
     :param request:
     """
-    if request.method == 'POST':
-        form = AddAlbumForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            file_name = str(request.FILES['album_file']).replace(' ', '_')
-            batch_params = str(request.FILES['album_file'])[:-5].split(' ')
-            block_num = batch_params[1]
-            batch_num = batch_params[-1]
-            try:
-                Block.objects.get(block_number=block_num, batch_number=batch_num)
-            except:
-                Block.objects.create(block_number=block_num, batch_number=batch_num)
-            block_id = Block.objects.get(block_number=block_num, batch_number=batch_num).pk
-            document_obj = open_file(file_name)
-            Album.objects.create(title='table1', content=handler(document_obj, 0, 2, 28, 1, 10), block_id=block_id)
-            Album.objects.create(title='table2_start', content=handler(document_obj, 1, 1, 11, 1, 5), block_id=block_id)
-            Album.objects.create(title='table2_100', content=handler(document_obj, 2, 1, 11, 1, 5), block_id=block_id)
-            Album.objects.create(title='table2_200', content=handler(document_obj, 3, 1, 11, 1, 5), block_id=block_id)
-            Album.objects.create(title='table2_300', content=handler(document_obj, 4, 1, 11, 1, 5), block_id=block_id)
-            Album.objects.create(title='table2_400', content=handler(document_obj, 5, 1, 11, 1, 5), block_id=block_id)
-            Album.objects.create(title='table2_500', content=handler(document_obj, 6, 1, 11, 1, 5), block_id=block_id)
-            Album.objects.create(title='table2_end', content=handler(document_obj, 7, 1, 11, 1, 5), block_id=block_id)
-            Album.objects.create(title='table3', content=handler(document_obj, 8, 2, 75, 1, 8), block_id=block_id)
-            Album.objects.create(title='table4', content=handler(document_obj, 9, 1, 28, 1, 15), block_id=block_id)
-            return redirect('bor_calc')
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AddAlbumForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                file_name = str(request.FILES['album_file']).replace(' ', '_')
+                batch_params = str(request.FILES['album_file'])[:-5].split(' ')
+                block_num = batch_params[1]
+                batch_num = batch_params[-1]
+                try:
+                    Block.objects.get(block_number=block_num, batch_number=batch_num)
+                except:
+                    Block.objects.create(block_number=block_num, batch_number=batch_num)
+                block_id = Block.objects.get(block_number=block_num, batch_number=batch_num).pk
+                document_obj = open_file(file_name)
+                Album.objects.create(title='table1', content=handler(document_obj, 0, 2, 28, 1, 10), block_id=block_id)
+                Album.objects.create(title='table2_start', content=handler(document_obj, 1, 1, 11, 1, 5), block_id=block_id)
+                Album.objects.create(title='table2_100', content=handler(document_obj, 2, 1, 11, 1, 5), block_id=block_id)
+                Album.objects.create(title='table2_200', content=handler(document_obj, 3, 1, 11, 1, 5), block_id=block_id)
+                Album.objects.create(title='table2_300', content=handler(document_obj, 4, 1, 11, 1, 5), block_id=block_id)
+                Album.objects.create(title='table2_400', content=handler(document_obj, 5, 1, 11, 1, 5), block_id=block_id)
+                Album.objects.create(title='table2_500', content=handler(document_obj, 6, 1, 11, 1, 5), block_id=block_id)
+                Album.objects.create(title='table2_end', content=handler(document_obj, 7, 1, 11, 1, 5), block_id=block_id)
+                Album.objects.create(title='table3', content=handler(document_obj, 8, 2, 75, 1, 8), block_id=block_id)
+                Album.objects.create(title='table4', content=handler(document_obj, 9, 1, 28, 1, 15), block_id=block_id)
+                return redirect('bor_calc')
+        else:
+            form = AddAlbumForm()
+        return render(request, 'bor_calculator/album_upload_page.html', {'title': 'Добавление альбома', 'form': form})
     else:
-        form = AddAlbumForm()
-    return render(request, 'bor_calculator/album_upload_page.html', {'title': 'Добавление альбома', 'form': form})
+        return redirect('login')
 
 
 def add_points(request):
